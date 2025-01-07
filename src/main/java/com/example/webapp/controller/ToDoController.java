7ed2a774ca9812f706057bcac5a08ac764b238ff
Hajime <eukaryotte314@gmail.com>
2025-01-07 11:51:22 +0900

    feat: フォームにバリデーションの機能を追加

--- src/main/java/com/example/webapp/controller/ToDoController.java
+++ src/main/java/com/example/webapp/controller/ToDoController.java
@@ -2,6 +2,8 @@ package com.example.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,7 +58,6 @@ public class ToDoController {
         }
     }
     
-	// === 登録・更新処理追加 ===
     /**
      * 新規登録画面を表示します。
      */
@@ -71,8 +72,16 @@ public class ToDoController {
      * 新規登録を実行します。
      */
     @PostMapping("/save")
-    public String create(ToDoForm form, 
-            RedirectAttributes attributes) {
+    public String create(@Validated ToDoForm form, 
+    		BindingResult bindingResult, 
+            	RedirectAttributes attributes) {
+		// === バリデーションチェック ===
+		// 入力チェックNG：入力画面を表示する
+		if (bindingResult.hasErrors()) {
+			// 新規登録画面の設定
+			form.setIsNew(true);
+			return "todo/form";
+		}
         // エンティティへの変換
         ToDo ToDo = ToDoHelper.convertToDo(form);
         // 登録実行
@@ -106,11 +115,19 @@ public class ToDoController {
     }
 
     /**
-     * 「すること」の情報を更新します。
+     * 「すること」を更新します。
      */
     @PostMapping("/update")
-    public String update(ToDoForm form, 
-            RedirectAttributes attributes) {
+    public String update(@Validated ToDoForm form, 
+    		BindingResult bindingResult, 
+            	RedirectAttributes attributes) {
+		// === バリデーションチェック ===
+		// 入力チェックNG：入力画面を表示する
+		if (bindingResult.hasErrors()) {
+			// 更新画面の設定
+			form.setIsNew(false);
+			return "todo/form";
+		}
         // エンティティへの変換
         ToDo ToDo = ToDoHelper.convertToDo(form);
         // 更新処理
@@ -120,7 +137,7 @@ public class ToDoController {
         // PRGパターン
         return "redirect:/todos";
     }
-    
+   
     /**
      * 指定されたIDの「すること」を削除します。
      */
